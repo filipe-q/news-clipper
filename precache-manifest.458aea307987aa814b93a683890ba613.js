0dc2d36e887ef9dd0c5fcbf7fe0e30558f7e732b
Filipe Quintas <filipe.quintas@dengun.com>
2020-05-03 15:54:32 +0100

    Updates

---
+++ precache-manifest.458aea307987aa814b93a683890ba613.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1626ac29289a6e499c366196bff083d",
+    "url": "/news-clipper/index.html"
+  },
+  {
+    "revision": "0685f8f8a8619d676ea7",
+    "url": "/news-clipper/static/css/2.3faf9ce9.chunk.css"
+  },
+  {
+    "revision": "ccd16c56dabb2469e510",
+    "url": "/news-clipper/static/css/main.a1a91cfe.chunk.css"
+  },
+  {
+    "revision": "0685f8f8a8619d676ea7",
+    "url": "/news-clipper/static/js/2.2d4d7902.chunk.js"
+  },
+  {
+    "revision": "d72c695bb2fed22c8cbf125cbcb911ef",
+    "url": "/news-clipper/static/js/2.2d4d7902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccd16c56dabb2469e510",
+    "url": "/news-clipper/static/js/main.8843f312.chunk.js"
+  },
+  {
+    "revision": "eaa3ecf8461b8e20a58b",
+    "url": "/news-clipper/static/js/runtime-main.9059f891.js"
+  }
+]);
